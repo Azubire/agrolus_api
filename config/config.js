7af78cdbde7352214c1003f4d386125f86442e83
Azubire Peter <azubirepeter@gmail.com>
2022-10-09 01:34:19 +0000

    -

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.Database_User,
-    password: process.env.Database_Pass,
-    database: process.env.Database_Name,
-    host: process.env.Database_Host,
+    // username: process.env.Database_User,
+    // password: process.env.Database_Pass,
+    // database: process.env.Database_Name,
+    // host: process.env.Database_Host,
+    username: "sql8525211",
+    password: "P8tIf3iEVY",
+    database: "sql8525211",
+    host: "sql8.freemysqlhosting.net",
     dialect: "mysql",
   },
   test: {
@@ -15,10 +19,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: "epiz_32753856",
+    password: "YKpmruUt2tW",
+    database: "epiz_32753856_agroplus",
+    host: "sql203.epizy.com",
     dialect: "mysql",
   },
 };
